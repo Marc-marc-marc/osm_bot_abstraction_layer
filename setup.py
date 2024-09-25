66190dc57c582070b27560596a877cefe4257f19
Marc-marc-marc <31963329+Marc-marc-marc@users.noreply.github.com>
2024-09-25 20:26:34 +0200

    avoir urllib v2
    urllib v2 need openssl update unable to compile on some venv

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         'osm_iterator>=1.5.0, <2.0',
         'tqdm>4.47.0, <5.0',
         'lxml>=3.5.0',
-        'urllib3>=1.25.8',
+        'urllib3>=1.25.8, <2.0',
         'taginfo>=0.0.3',
         # webbrowser is  part of stdlib https://stackoverflow.com/a/47926698
     ],
